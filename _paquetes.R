f9b6981bf27774fb2c1412d959c494b2fecafb1f
William Chavarría <wchavarria@tigo.com.gt>
2023-10-20 12:44:51 -0600

    Cambio de parqueta a top20 celdas

--- _paquetes.R
+++ _paquetes.R
@@ -4,11 +4,14 @@ import::from(zeallot,       `%<-%`)
 import::from(fs,            dir_ls)
 import::from(parallel,      detectCores, makePSOCKcluster, stopCluster)
 import::from(doParallel,    registerDoParallel)
-import::from(conectigo,     conectar_msql)
+import::from(conectigo,     conectar_msql, cargar_fuentes)
 import::from(DBI,           dbDisconnect)
 import::from(dbplyr,        in_schema)
+import::from(FSelectorRcpp, information_gain)
+import::from(cowplot,       .except = "stamp")
 
 pacman::p_load(
+ fst,
  tictoc,
  themis,
  janitor,
@@ -19,17 +22,47 @@ pacman::p_load(
  arrow,
  colino,
  textrecipes,
+ spatialsample,
  tidymodels,
  tidyverse)
 
 
 options(pillar.sigfig    = 5,
-        tibble.print_min = 10,
+        tibble.print_min = 50,
         scipen = 999,
         digits = 7,
         tidymodels.dark = TRUE,
         readr.show_col_types = FALSE,
         dplyr.summarise.inform = FALSE)
+
+
+cargar_fuentes()
+
+yunkel <- theme_cowplot(font_family = "yano") +
+  theme(
+    plot.margin          = unit(c(10, 2, 2, 2), "mm"),
+    axis.title           = element_text(size = 30),
+    axis.text            = element_text(size = 20),
+    plot.title           = element_text(size = 50),
+    plot.subtitle        = element_text(size = 18),
+    legend.position      = "bottom",
+    legend.text          = element_text(size = 20),
+    legend.justification = "center",
+    legend.title         = element_blank())
+
+# tema con grid horizontal y vertical
+drako <- theme_bw(base_family = "yano", base_size = 14) +
+  theme(
+    plot.margin = unit(c(10, 2, 2, 2), "mm"),
+    axis.title    = element_text(size = 30),
+    axis.text     = element_text(size = 20),
+    plot.title    = element_text(size = 50),
+    plot.subtitle = element_text(size = 18),
+    legend.title  = element_blank())
+
+theme_set(yunkel)
+
+
 
 # preparar y desplegar receta
 ver <- . %>% prep() %>% juice()
@@ -71,3 +104,121 @@ ajustar <- function() {
   pivot_wider(names_from = .metric, values_from = mean)
 
 }
+
+
+# Función para sumarizar métricas
+summarize_metrics <- function(df) {
+ df |>
+  summarise(
+
+  twr = first(twr),
+  dpto = first(dpto),
+  ciudad = first(ciudad),
+
+  prb_mean = mean(prb),
+  prb_median = median(prb),
+  prb_min = min(prb),
+  prb_max = max(prb),
+  prb_sd = sd(prb),
+  prb_out_of_range_time = mean(prb > 0.85) * 100,
+  prb_out_of_range_count = sum(prb > 0.85),
+  prb_out_of_range_bin = as.integer(mean(prb > 0.85) * 100 >= 3),
+
+  load_mean = mean(rrc),
+  load_median = median(load),
+  load_min = min(load),
+  load_max = max(load),
+  load_sd = sd(load),
+  load_out_of_range_time = mean(load > 0.77) * 100,
+  load_out_of_range_count = sum(load > 0.77),
+  load_out_of_range_bin = as.integer(mean(load > 0.77) * 100 >= 3),
+
+  rrc_mean = mean(rrc),
+  rrc_median = median(rrc),
+  rrc_min = min(rrc),
+  rrc_max = max(rrc),
+  rrc_sd = sd(rrc),
+  rrc_out_of_range_time = mean(rrc < 0.9) * 100,
+  rrc_out_of_range_count = sum(rrc < 0.9),
+  rrc_out_of_range_bin = as.integer(mean(rrc < 0.9) * 100 >= 3),
+
+  erab_mean = mean(erab),
+  erab_median = median(erab),
+  erab_min = min(erab),
+  erab_max = max(erab),
+  erab_sd = sd(erab),
+  erab_out_of_range_time = mean(erab < 0.9) * 100,
+  erab_out_of_range_count = sum(erab < 0.9),
+  erab_out_of_range_bin = as.integer(mean(erab < 0.9) * 100 >= 3),
+
+  dropr_mean = mean(dropr),
+  dropr_median = median(dropr),
+  dropr_min = min(dropr),
+  dropr_max = max(dropr),
+  dropr_sd = sd(dropr),
+  dropr_out_of_range_time = mean(dropr > 0.015) * 100,
+  dropr_out_of_range_count = sum(dropr > 0.015),
+  dropr_out_of_range_bin = as.integer(mean(dropr > 0.015) * 100 >= 3),
+
+  interf_mean = mean(interf),
+  interf_median = median(interf),
+  interf_min = min(interf),
+  interf_max = max(interf),
+  interf_sd = sd(interf),
+  interf_out_of_range_time = mean(interf > -95) * 100,
+  interf_out_of_range_count = sum(interf > -95),
+  interf_out_of_range_bin = as.integer(mean(interf > -95) * 100 >= 3),
+
+  thp_dl_mean = mean(thp_dl),
+  thp_dl_median = median(thp_dl),
+  thp_dl_min = min(thp_dl),
+  thp_dl_max = max(thp_dl),
+  thp_dl_sd = sd(thp_dl),
+  thp_dl_out_of_range_time = mean(thp_dl < 2.7) * 100,
+  thp_dl_out_of_range_count = sum(thp_dl < 2.7),
+  thp_dl_out_of_range_bin = as.integer(mean(thp_dl < 2.7) * 100 >= 3),
+
+  cqi_mean = mean(cqi),
+  cqi_median = median(cqi),
+  cqi_min = min(cqi),
+  cqi_max = max(cqi),
+  cqi_sd = sd(cqi),
+  cqi_out_of_range_time = mean(cqi < 7) * 100,
+  cqi_out_of_range_count = sum(cqi < 7),
+  cqi_out_of_range_bin = as.integer(mean(cqi < 7) * 100 >= 3),
+
+  ta_mean = mean(ta),
+  ta_median = median(ta),
+  ta_min = min(ta),
+  ta_max = max(ta),
+  ta_sd = sd(ta),
+  ta_out_of_range_time = mean(ta > 0.15) * 100,
+  ta_out_of_range_count = sum(ta > 0.15),
+  ta_out_of_range_bin = as.integer(mean(ta > 0.15) * 100 >= 3),
+
+  lat = first(lat),
+  lon = first(lon),
+
+  diag = first(diag)
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ctl_por_hora.R
+++ ctl_por_hora.R
@@ -20,36 +20,35 @@ etiquetas <- c(
  "NO_DIAG")
 
 lookup <- c(
- fecha          = "fecha_15dias",
- interf         = "l_ul_interference_avg",
- usuario        = "msisdn_dd",
- ciudad         = "cty_nm",
- depto          = "stt_nm",
- region         = "trrtry_tf_nm",
- erab           = "erab_success_rate",
- mod_16qam      = "modulation_16qam_ratio",
- mod_64qam      = "modulation_64qam_ratio",
- mod_qpsk       = "modulation_qpsk_ratio",
- bts            = "bts_sh_nm",
- disp_ttks      = "avlblty_tckt_2hrs",
- disp           = "avlblty",
- disp_prop      = "unvlblty_ttl_hrs_prop",
- rrc            = "rrc_success_rate",
- drop_rate      = "service_drop_rate",
- thp_lte        = "thp_required_lte",
- ta_1           = "ra_ta_ue_index1",
- ta_2           = "ra_ta_ue_index2",
- ta_3           = "ra_ta_ue_index3",
- ta_4           = "ra_ta_ue_index4",
- ta_5           = "ra_ta_ue_index5",
- ta_6           = "ra_ta_ue_index6",
- ta_7           = "ra_ta_ue_index7",
- ta_total       = "ra_ta_ue_total",
- prb            = "rate_prb_dl",
- cqi            = "corrected_cqi",
- thp_dl         = "thoughput_dl",
- lluvia         = "rain",
- tiempo         = "cll_prctg"
+ # date   = "fecha_metrica",
+ interf  = "l_ul_interference_avg",
+ user    = "msisdn_dd",
+ erab    = "erab_success_rate",
+ # m16qam = "modulation_16qam_ratio",
+ # m64qam = "modulation_64qam_ratio",
+ # mqpsk  = "modulation_qpsk_ratio",
+ bts       = "bts_sh_nm",
+ load      = "cell_load",
+ # disp_ttks = "avlblty_tckt_2hrs",
+ # disp      = "avlblty",
+ # disp_prop = "unvlblty_ttl_hrs_prop",
+ rrc       = "rrc_success_rate",
+ dropr     = "service_drop_rate",
+ # thp_lte   = "thp_required_lte",
+ # ta1       = "ra_ta_ue_index1",
+ # ta2       = "ra_ta_ue_index2",
+ # ta3       = "ra_ta_ue_index3",
+ # ta4       = "ra_ta_ue_index4",
+ # ta5       = "ra_ta_ue_index5",
+ ta6       = "ra_ta_ue_index6",
+ ta7       = "ra_ta_ue_index7",
+ # ta_to     = "ra_ta_ue_total",
+ prb       = "rate_prb_dl",
+ cqi       = "corrected_cqi",
+ thp       = "thoughput_dl"
+ # thp_ul    = "thpughput_ul"
+ # lluvia  = "rain",
+ # time      = "cll_prctg"
 )
 
 ##  ............................................................................
@@ -60,276 +59,314 @@ archivos <- dir_ls(type = "file", path = "data", glob = "*.csv")
 c(diag_00, disp_00, metr_00, lluv_00) %<-% (archivos |> map(read_csv))
 
 cols_to_remove <- c(
-        "tipo_cobertura",   # 97.7% de los datos dicen TBD y el 2% es NA
-        "cell_load_3g",     # André indica: "mejor utilizar `cell_load_lte`
-        "users_class",      # Ignorar por el momento
-        "fct_dt",           # Actualización del registro a nivel de data
-        "time_3g",          # Métrica vieja que ya no se le dio seguimiento
-        "bs_ln_nm",         # Dejaremos `bs_ln_nm` por contener más información
-        "thp_required_3g",  # Demasiados valores faltantes
-        "time_lte",         # Demasiados valores faltantes,
-        "bs_ln_cd",         # Feature no informativo
-        "nps_mdll",         # Genera fuga de datos,
-        "bts_sh_nm_1",      # Mejor usar bts_sh_nam
-        "cell_load_lte",    # La métrica time_cl y time_lte son mejores
-        "trrtry_cmrcl_nm",  # No determina la ubicación de la celda
-        "stt_nm",           # No determina la ubicación de la celda
-        "trrtry_tf_nm",     # No determina la ubicación de la celda
-        "cty_nm",           # No determina la ubicación de la celda
-        "thp_required_lte"  # Métrica sumarizada
+ "tipo_cobertura",   # 97.7% de los datos dicen TBD y el 2% es NA
+ "cell_load_3g",     # André indica: "mejor utilizar `cell_load_lte`
+ "users_class",      # Ignorar por el momento
+ "fct_dt",           # Actualización del registro a nivel de data
+ "time_3g",          # Métrica vieja que ya no se le dio seguimiento
+ "bs_ln_nm",         # Dejaremos `bs_ln_nm` por contener más información
+ "thp_required_3g",  # Demasiados valores faltantes
+ "time_lte",         # Demasiados valores faltantes,
+ "bs_ln_cd",         # Feature no informativo
+ "nps_mdll",         # Genera fuga de datos,
+ "bts_sh_nm_1",      # Mejor usar bts_sh_nam
+ "cell_load_lte",    # La métrica time_cl y time_lte son mejores
+ "trrtry_cmrcl_nm",  # No determina la ubicación de la celda
+ "stt_nm",           # No determina la ubicación de la celda
+ "trrtry_tf_nm",     # No determina la ubicación de la celda
+ "cty_nm",           # No determina la ubicación de la celda
+ "thp_required_lte"  # Métrica sumarizada
 )
-
 
 # Métricas por hora ------------------------------------------------------------
 
 parquet_file <- dir_ls(type = "file", path = "data", glob = "*.parquet")
 
-# 659,966 x 28
-kpis_00 <- read_parquet(file = parquet_file)
-
+tic()
+# 11,785,380 × 28 (3.36 seg)
+hourly_metrics_raw <- read_parquet(file = parquet_file)
+toc()
 
 # Sitios Field Service ---------------------------------------------------------
 
 con <- conectar_msql()
 
 sitiosfs <- tbl(con, in_schema("fieldservice", "site_collate")) |>
-  select(twr = cilocation, status, lat = latitude, lon = longitude,
-         dpto = department, municipalidad = municipality) %>%
-  collect() |>
-  distinct(twr, .keep_all = TRUE)
+ select(
+  twr = cilocation,
+  lat = latitude,
+  lon = longitude,
+  dpto = department,
+  ciudad = municipality) |>
+ collect() |>
+ distinct(twr, .keep_all = TRUE)
 
 dbDisconnect(con)
 
 ##  ............................................................................
 ##  Prep                                                                    ####
 
-
 # Diagnóstico ------------------------------------------------------------------
 
-# 72,644 × 6
-diag_01 <- diag_00 |>
- select(
-  where(
-   \(x) length(unique(x)) != 1 &&
-   mean(is.na(x)) < 0.5),
-   -all_of(cols_to_remove)
- ) |>
+# # Preprocesamiento y filtrado inicial
+diagnosticos <- diag_00 |>
+ select(fct_srvy_dt, msisdn_dd, srvy_id, class_desc, diag) |>
  mutate(
   across(fct_srvy_dt, \(fecha) ymd(fecha)),
-  across(where(is.character), \(col) estandarizar_columnas(col)),
-  across(msisdn_dd:srvy_id, as.integer)
- ) |>
- relocate(fct_srvy_dt, msisdn_dd, srvy_id)
-
-# Segunda transformación
-diag_02 <- diag_01 |>
- arrange(fct_srvy_dt, msisdn_dd, srvy_id) |>
- mutate(
-  across(where(is.character), \(x) na_if(x, "TBD")),
-  across(where(is.character), \(x) na_if(x, "TO_BE_DETERMINED")))
-
-# Tercera transformación: 6,587 x 8
-diag_03 <- diag_02 |>
+  across(msisdn_dd:srvy_id, as.integer),
+  across(diag, toupper)) |>
  filter(diag %in% etiquetas) |>
+
+ # Dividir por 'diag' y aplicar transformaciones al grupo "NO_DIAG"
  split(~ diag) |>
  map_at("NO_DIAG", \(df) df |>
  filter(class_desc == "PROMOTOR") |>
  drop_na() |>
  slice_sample(n = 1800) |>
  distinct(msisdn_dd, .keep_all = TRUE)) |>
+
+ # Combinar listas en un solo data frame
  list_rbind() |>
+
+ # Transformaciones finales
  mutate(
   across(diag,
   \(x) case_match(x, "NO_DIAG" ~ "PROMOTOR", .default = diag))) |>
   select(-class_desc) |>
  distinct(msisdn_dd, .keep_all = TRUE)
 
+# Métricas por hora ------------------------------------------------------------
 
-# Métricas resumidas -----------------------------------------------------------
+metrics_to_remove <- c(
+ "r1",
+ "prttn_hr",
+ "hora",
+ "cll_prctg",
+ "ra_ta_ue_index1",
+ "ra_ta_ue_index2",
+ "ra_ta_ue_index3",
+ "ra_ta_ue_index4",
+ "ra_ta_ue_index5",
+ "fecha_metrica",
+ "ra_ta_ue_total",
+ "modulation_64qam_ratio",
+ "modulation_16qam_ratio",
+ "modulation_qpsk_ratio",
+ "thpughput_ul",
+ "volte_erlang")
 
-
-excluir_metricas <- c("cell_load", "thpughput_ul", "volte_erlang")
-
-# 273,434 x 24
-metr_01 <- metr_00 |>
- mutate(across(fct_srvy_dt, ymd)) |>
- relocate(fct_srvy_dt, msisdn_dd, srvy_id, r1, bts_sh_nm) |>
- arrange(fct_srvy_dt, msisdn_dd) |>
- select(-all_of(excluir_metricas))
-
-metr_01 |>
- filter(msisdn_dd == 31324863) |>
- select(fct_srvy_dt, msisdn_dd, bts_sh_nm, cll_prctg, rate_prb_dl)
-
-
-# Métricas detalle -------------------------------------------------------------
-
-# 659,966 x 30
-kpis_01 <- kpis_00 |>
- separate_wider_position(prttn_hr, c(fecha_metrica = 8, hora = 2)) |>
+# 29.64 seg
+tic()
+# 11,589,585 × 30
+hourly_metrics <- hourly_metrics_raw |>
+ mutate(
+  fecha_metrica = str_sub(prttn_hr, 1, 8),
+  hora = str_sub(prttn_hr, 9, 10)) |>
  mutate(
   fecha_metrica = ymd(fecha_metrica),
-  timestamp = str_c(fecha_metrica, hora, sep = " ") |> ymd_h(),
+  date = str_c(fecha_metrica, hora, sep = " ") |> ymd_h(),
   across(fct_srvy_dt, ymd),
-  across(msisdn_dd, as.integer)
+  across(c(msisdn_dd, hora), as.integer)
  ) |>
- relocate(hora, timestamp, .after = srvy_id)
+ filter(corrected_cqi > 5) |>
+ relocate(hora, date, .after = srvy_id) |>
+ select(-all_of(metrics_to_remove))
+toc()
 
-# 483,933 × 35
-kpis_02 <- diag_03 |>
- inner_join(kpis_01, join_by(fct_srvy_dt, msisdn_dd, srvy_id))
+# Unir métricas detalle con diagnóstico ----------------------------------------
 
-kpis_03 <- kpis_02 |>
- select(fct_srvy_dt:cty_nm, bts_sh_nm) |>
- mutate(twr = str_sub(bts_sh_nm, start = 2, end = 7)) |>
- left_join(sitiosfs, join_by(twr))
+# 39.27 seg
+tic()
+# Unir diagnósticos con métricas de red
+ctl <- diagnosticos |>
+ inner_join(hourly_metrics, join_by(fct_srvy_dt, msisdn_dd, srvy_id)) |>
 
-## Mapa
+# Extraer el siteid y agregar información de ubicación
+ mutate(
+  twr = str_sub(bts_sh_nm, start = 2, end = 7)) |>
+ left_join(sitiosfs, join_by(twr)) |>
 
-theme_set(theme_light())
-library(ggthemes)
+# Renombrar y remover columnas de enlace
+ rename(all_of(lookup)) |>
+ select(-c(fct_srvy_dt, srvy_id)) |>
 
-mapa <- diag_03 |>
- inner_join(lluv_01, join_by(fct_srvy_dt, msisdn_dd, srvy_id))
+# Remover filas que tengan valor cero en varias columnas consecutivas
+filter(
+ !if_all(
+  c(prb, load, dropr, interf, thp), ~ .x == 0)) |>
 
-rangos <- map_data("world") |>  as_tibble() |>
- filter(region == "Guatemala")  |>
- reframe(rango_lat = range(lat), rango_lon = range(long))
+# Transformaciones finales de formato y localización de columnas
+ mutate(
+  across(where(is_character), \(col) estandarizar_columnas(col)),
+  across(diag, as_factor)) |>
+ relocate(user, where(is.POSIXct), where(is_character),
+  where(is.numeric), where(is.factor)) |>
+ rowwise() |>
+ mutate(ta = ta6 + ta7, .keep = "unused", .after = cqi) |>
+ ungroup() |>
+ arrange(user, desc(date))
+toc()
+# 8,383,655 × 18
 
+# Escribir en un fst para acceso rápido futuro
+# write_fst(ctl, "./preprocessed_data/ctl_final.fst", compress = 0)
 
-just_gt <- kpis_03 |>
- filter(stt_nm == "GUATEMALA") |>
- slice_sample(n = 5000) |>
- drop_na()
-
-just_gt |>
-ggplot(aes(lon, lat)) +
- geom_point() +
- borders(regions = "Guatemala") +
- theme_map()
-
-
-just_gt |>
- filter(str_detect(twr, "^GUA|^GTA", negate = TRUE))
-
-
-## Mapa
-
-theme_set(theme_light())
-library(ggthemes)
-
-mapa <- diag_03 |>
- inner_join(lluv_01, join_by(fct_srvy_dt, msisdn_dd, srvy_id))
-
-rangos <- map_data("world") |>  as_tibble() |>
- filter(region == "Guatemala")  |>
- reframe(rango_lat = range(lat), rango_lon = range(long))
+# 1.98 seg
+ctl <- read_fst("./preprocessed_data/ctl_final.fst") |> as_tibble()
 
 
-just_gt <- mapa |>
- filter(stt_nm == "GUATEMALA") |>
- slice_sample(n = 5000) |>
- drop_na()
+
+# craft -------------------------------------------------------------------
+
+muestra <- c(30001118, 30003353)
+
+anomalias <- tribble(
+  ~user, ~date, ~prb, ~thp,
+  30001118, "2023-07-03 23:00:00", 0.90, 1900,
+  30001118, "2023-07-04 23:00:00", 0.91, 1300,
+  30001118, "2023-07-05 23:00:00", 0.92, 500,
+  30001118, "2023-07-06 22:00:00", 0.93, 800,
+  30001118, "2023-07-07 10:00:00", 0.94, 1241,
+  30001118, "2023-07-08 11:00:00", 0.95, 241,
+  30001118, "2023-07-09 12:00:00", 0.95, 341,
+  30001118, "2023-07-10 13:00:00", 0.95, 141,
+  30003353, "2023-07-03 23:00:00", 0.20, 2400,
+  30003353, "2023-07-04 23:00:00", 0.30, 2300,
+  30003353, "2023-07-05 23:00:00", 0.40, 2200,
+  30003353, "2023-07-06 22:00:00", 0.50, 2100,
+  30003353, "2023-07-07 22:00:00", 0.60, 2000
+) |> mutate(date = ymd_hms(date))
+
+prueba <- ctl |>
+  filter(user %in% muestra) |>
+  group_by(user) |>
+  slice(1:10) |>
+  ungroup() |>
+  select(user, date, prb, thp) |>
+  bind_rows(anomalias) |>
+  arrange(user, date)
+
+prueba |>
+
+  group_by(user) |>
+  mutate(
+    prb = prb * 100,
+    thp = thp/1e3,
+    prb_ratio = (prb / thp),
+    prb_out_range = if_else(condition = prb_ratio > 32, 1, 0)) |>
+  summarise(
+    sum_prb_ratio  = sum(prb_out_range),
+    capacity_issue = ifelse(sum_prb_ratio > 5, 1, 0))
 
 
-ggplot(aes(lon, lat)) +
- geom_point() +
- borders(regions = "Guatemala") +
- # scale_size_area() +
- # coord_quickmap() +
- theme_map()
-
-map_data("world") |>
- as_tibble() |>
- count(region) |> print(n = Inf)muestra <- c(
-        30299503,
-        31462506,
-        32063433,
-        55261666,
-        31530772,
-        45644345,
-        53596846,
-        46915988,
-        31139092,
-        38369266)
-
-# 30299503 (promotor)
-# 31462506 (promotor)
-
-# 32063433 (optimización)
-# 55261666 (optimización)
-
-# 31530772 (disponibilidad)
-# 45644345 (disponibilidad)
-
-# 53596846 (cobertura)
-# 46915988 (cobertura)
-
-# 31139092 (capacidad)
-# 38369266 (capacidad)
-
-
-diag_x <- diag_03 |>
- filter(msisdn_dd %in% muestra) |>
- select(fct_srvy_dt, msisdn_dd, srvy_id, diag)
-#
-usuario_x <- kpis_01 |>
- filter(msisdn_dd %in% muestra) |>
- select(fct_srvy_dt, timestamp, msisdn_dd, srvy_id, bts_sh_nm,
-        fecha_metrica, hora, cll_prctg, rate_prb_dl) |>
- mutate(across(msisdn_dd, as.integer),
-        across(fct_srvy_dt, ymd)) |>
- arrange(timestamp)
-
-
-review <- diag_x |>
- left_join(usuario_x, join_by(msisdn_dd, srvy_id, fct_srvy_dt)) |>
- relocate(diag, .after = last_col())
-
-
-review |> inspectdf::inspect_na()
-review
-
-review |>
- group_by(msisdn_dd) |>
- summarise(across(bts_sh_nm:hora, n_distinct),
-           diag = first(diag)) |>
- relocate(diag, .after = msisdn_dd) |>
- arrange(diag)
-
-review |>
- group_by(msisdn_dd, fecha_metrica) |>
- summarise(across(c(bts_sh_nm, hora), n_distinct),
-                  diag = first(diag)) |>
- relocate(diag, .after = msisdn_dd) |>
- arrange(diag) |>
- ungroup()
-
-
-## Disponibilidad
-
-disp_01 <- disp_00 |>
- mutate(across(everything(), \(x) na_if(x, -1))) |>
- select(-time_thrgpt)
+# 1. Pasar todas las unidades a porcentaje y no a proporción: multiplicar x 100
+# 2. Pasar el thp a mbps
+# 3. Crear los nuevos features con base a las reglas de ctl
 
 
 
 
-ctl_00 <- metr_01 |>
- inner_join(disp_01, join_by(msisdn_dd, srvy_id)) |>
- inner_join(lluv_01, join_by(msisdn_dd, srvy_id, fct_srvy_dt, bts_sh_nm))
+##  ............................................................................
+##  Resumir                                                                 ####
+
+# Crear dos datasets
+
+tic()
+# 24 seg
+ctl_per_cell <- ctl |>
+  group_by(user, bts) |>
+  summarize_metrics() |>
+  ungroup()
+toc()
+# 16,252 × 80
+
+tic()
+# 7.51 seg
+ctl_per_user <- ctl |>
+  group_by(user) |>
+  summarize_metrics() |>
+  ungroup()
+toc()
+# 4,225 × 79
+
+##  ............................................................................
+##  Split                                                                   ####
 
 
 
 
-tbl(con, in_schema("fieldservice", "directory_siteattribute"))
+##  ............................................................................
+##  EDA                                                                     ####
 
-## Lluvia
+mi_per_cell <- information_gain(
+  formula      = diag ~ .,
+  data         = ctl_per_cell[-1],
+  type         = "infogain",
+  discIntegers = FALSE) |>
+  as_tibble() |>
+  arrange(-importance) |>
+  rename(per_cell = importance)
 
-lluv_01 <- lluv_00 |>
- select(-fct_srvy_15_mnth) |>
- mutate(across(fct_srvy_dt, ymd)) |>
- left_join(sitiosfs, join_by(twr))
+mi_per_user <- information_gain(
+  formula      = diag ~ .,
+  data         = ctl_per_user[-1],
+  type         = "infogain",
+  discIntegers = FALSE) |>
+  as_tibble() |>
+  arrange(-importance) |>
+  rename(per_user = importance)
+
+
+mi_per_cell |>
+  mutate(feature = fct_reorder(attributes, importance, .desc = FALSE)) |>
+  ggplot(aes(x = importance, y = feature)) +
+  geom_point() +
+  labs(title = "MI por detalle de celda") +
+  drako +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10))
+
+
+mi_per_user |>
+  mutate(feature = fct_reorder(attributes, importance, .desc = FALSE)) |>
+  ggplot(aes(x = importance, y = feature)) +
+  geom_point() +
+  labs(title = "MI por usuario") +
+  drako +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10))
+
+
+mi_total <- mi_per_cell |>
+  left_join(mi_per_user, join_by(attributes))
+
+
+
+
+cubist_spec <-
+  cubist_rules(committees = tune(), neighbors = tune()) %>%
+  set_engine("Cubist")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
