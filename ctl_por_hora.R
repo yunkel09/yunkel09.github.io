2d9f39d7894381c6a13d787a93ede0dba600181f
William Chavarría <wchavarria@tigo.com.gt>
2023-10-23 02:39:45 -0600

    Crear reglas de etiquetado

--- ctl_por_hora.R
+++ ctl_por_hora.R
@@ -17,8 +17,8 @@ etiquetas <- c(
  "CAPACIDAD",
  "OPTIMIZACION",
  "COBERTURA",
- "DISPONIBILIDAD",
- "NO_DIAG")
+ "DISPONIBILIDAD")
+ # "NO_DIAG")
 
 # Tabla par renombrar columnas
 lookup <- c(
@@ -32,28 +32,34 @@ lookup <- c(
   drp   = "service_drop_rate",
   prb   = "rate_prb_dl",
   cqi   = "corrected_cqi",
+  lte   = "time_lte",
   thp   = "thoughput_dl",
-  dis   = "cell_unavail",
-  dif   = "cell_unavail_s1fail")
+  m64   = "modulation_64qam_ratio",
+  m16   = "modulation_16qam_ratio",
+  psk   = "modulation_qpsk_ratio",
+  time  = "cll_prctg",
+  vol   = "volte_erlang",
+  dis   = "cell_unavail")
 
 # Remover de métricas
 metrics_to_remove <- c(
   "srvy_id",
-  # "r1",
+  "r1",
   "prttn_hr",
-  "status",
+  # "status",
   # "cll_prctg",
   "ra_ta_ue_index1",
   "ra_ta_ue_index2",
   "ra_ta_ue_index3",
   "ra_ta_ue_index4",
   "ra_ta_ue_index5",
+  "ra_ta_ue_index6",
+  "ra_ta_ue_index7",
   "ra_ta_ue_total",
-  "modulation_64qam_ratio",
-  "modulation_16qam_ratio",
-  "modulation_qpsk_ratio",
   "thpughput_ul",
-  "volte_erlang")
+  "cell_unavail_s1fail"
+  # "volte_erlang"
+  )
 
 enteros <- c(
  "msisdn_dd",
@@ -66,8 +72,15 @@ porcentajes <-c(
  "cell_load",
  "rrc_success_rate",
  "erab_success_rate",
+ "modulation_64qam_ratio",
+ "modulation_16qam_ratio",
+ "modulation_qpsk_ratio",
  "service_drop_rate",
+ "time_lte",
+ "cll_prctg",
  "tad")
+
+sitios_ruedas <- c("COW990", "COW994", "COW995", "COW999")
 
 ##  ............................................................................
 ##  Ingesta                                                                 ####
@@ -85,7 +98,6 @@ cols_diag <- cols_only(
  fct_srvy_dt = "c",
  msisdn_dd   = "c",
  srvy_id     = "i",
- class_desc  = "c",
  time_lte    = "d",
  diag        = "c")
 
@@ -99,11 +111,10 @@ hourly_metrics_raw <- read_fst(path = metr_f) |> as_tibble()
 
 # Sitios Field Service ---------------------------------------------------------
 
-
 con <- conectigo::conectar_msql()
 
 sitiosfs_raw <- tbl(con, in_schema("fieldservice", "site_collate")) |>
-  select(twr = cilocation, status, lat = latitude, lon = longitude,
+  select(twr = cilocation, lat = latitude, lon = longitude,
          dpto = department, municipalidad = municipality) %>%
   collect() |>
   distinct(twr, .keep_all = TRUE)
@@ -111,10 +122,6 @@ sitiosfs_raw <- tbl(con, in_schema("fieldservice", "site_collate")) |>
 sitiosfs <- sitiosfs_raw |>
   mutate(across(dpto:municipalidad, \(x) estandarizar_columnas(x))) |>
   rename(city = "municipalidad")
-
-
-# 5,412
-# sitiosfs <- read_fst(sifs_f) |> as_tibble()
 
 ##  ............................................................................
 ##  Prep                                                                    ####
@@ -125,269 +132,85 @@ sitiosfs <- sitiosfs_raw |>
 diagnosticos <- diag_00 |>
  mutate(
   across(diag, toupper)) |>
- filter(diag %in% etiquetas) |>
-
- # Dividir por 'diag' y aplicar transformaciones al grupo "NO_DIAG"
- split(~ diag) |>
- map_at("NO_DIAG", \(df) df |>
- filter(class_desc == "PROMOTOR") |>
- drop_na() |>
- # slice_sample(n = 1800) |>
- distinct(msisdn_dd, .keep_all = TRUE)) |>
-
- # Combinar listas en un solo data frame
- list_rbind() |>
-
- # Transformaciones finales
- mutate(
-  across(diag,
-  \(x) case_match(x, "NO_DIAG" ~ "PROMOTOR", .default = diag))) |>
-  select(-class_desc) |>
- distinct(msisdn_dd, .keep_all = TRUE)
+ filter(diag %in% etiquetas)
 
 # Unir y transformar -----------------------------------------------------------
 
 # 18.2 seg
 ctl_00 <- diagnosticos |>
-  inner_join(hourly_metrics_raw, join_by(fct_srvy_dt, msisdn_dd, srvy_id))
-# 32,164,414 × 31
-
-ctl_00 |>
-  arrange(msisdn_dd, r1, cll_prctg)
-
-
+ inner_join(
+  hourly_metrics_raw, join_by(fct_srvy_dt, msisdn_dd, srvy_id),
+  relationship = "many-to-many")
 
 # Dataset exploratorio ---------------------------------------------------------
 
-sitios_ruedas <- c("COW990", "COW994", "COW995", "COW999")
-
-# 529 seg (8.33 min)
+# 327 seg (5 min)
 tic()
 ctl_01 <- ctl_00 |>
- filter(
-
-  # Eliminar casos con contadores apagados
-  !if_all(
-   c(rate_prb_dl,
-     cell_load,
-     service_drop_rate,
-     l_ul_interference_avg,
-     thoughput_dl), ~ .x == 0),
-
-  # Los cqis menores a 5 no son de casos normales
-  corrected_cqi > 5
-
- ) |>
- # Crear columna especial
- rowwise() |>
- mutate(tad = ra_ta_ue_index6 + ra_ta_ue_index7, .keep = "unused") |>
- ungroup() |>
-
- # Crear y transformar columnas
  mutate(
-
-  # fechas
-  met = str_sub(prttn_hr, 1, 8) |> ymd(),
-  hor = str_sub(prttn_hr, 9, 10),
-  date = str_c(met, hor, sep = " ") |> ymd_h(),
-
-  # nuevas
-  twr  = str_sub(bts_sh_nm, start = 2, end = 7),
-
-  # conversiones
-  across(fct_srvy_dt, ymd),                      # convertir a fecha
-  across(all_of(enteros), as.integer),           # convertir a enteros
-  across(thoughput_dl, \(x) x / 1e3),            # convertir a Mbps
-  across(all_of(porcentajes), \(col) col * 100), # convertir a porcentaje
-  across(diag, as_factor)                        # convertir a factor
-
- ) |>
-
- # Agregar datos de geografía
+  tad   = ra_ta_ue_index6 + ra_ta_ue_index7,
+  fecha = str_sub(prttn_hr, 1, 8) |> ymd(),
+  hora  = str_sub(prttn_hr, 9, 10),
+  date  = str_c(fecha, hora, sep = " ") |> ymd_h(),
+  twr   = str_sub(bts_sh_nm, start = 2, end = 7),
+  across(all_of(enteros), as.integer),
+  across(thoughput_dl, \(x) x / 1e3),
+  across(all_of(porcentajes), \(col) col * 100)) |>
  left_join(sitiosfs, join_by(twr)) |>
-
- # Remover columnas innecesarias y luego renombrar
  select(-all_of(metrics_to_remove)) |>
  rename(any_of(lookup)) |>
-
- # Reordenar dataset
  relocate(
-  user,
-  date,
-  poll,
-  met,
-  hor,
-  dpto,
-  city,
-  twr,
-  bts,
-  lat,
-  lon,
-  prb,
-  thp,
-  rrc,
-  erb,
-  drp,
-  tad,
-  lod,
-  erf,
-  cqi,
-  dis,
-  dif,
-  diag) |>
- filter(!twr %in% sitios_ruedas)
+  user, date, poll, fecha, hora, dpto, city,twr, bts, time, lat, lon, prb,
+  thp, rrc,erb, drp, tad, lod, erf, cqi, m64, m16, psk, vol, dis, lte, diag)
 toc()
-# 31,648,712 × 23
+
+tic()
+ctl_02 <- ctl_01 |>
+ filter(
+  !twr %in% sitios_ruedas,
+  !if_all(prb:dis, ~ .x == 0)) |>
+ arrange(user, fecha, hora, desc(time)) |>
+ group_by(user, fecha, hora) |>
+ slice_head(n = 5) |>
+ ungroup()
+toc()
+# 11,044,464 × 24
 
 # Guardar dataset preprocesado
 cted_f <- path("data", "ctlexp", ext = "fst")
-write_fst(ctl_01, path = cted_f, compress = 0)
+write_fst(ctl_02, path = cted_f, compress = 0)
+
+ctl_02 <- read_fst(cted_f) |> as_tibble()
 
 
-# Crear dos datasets
-tic()
-# 719 seg (11 min)
-c(top3, top5, top10) %<-% (c(3, 5, 10) |>
- map(~ ctl_01 |>
-  arrange(user, met, hor, desc(cll_prctg)) |>
-  group_by(user, met, hor) |>
-  slice_head(n = .x) |>
-  mutate(top = row_number()) |>
-  ungroup()))
-toc()
+ctl_02 |> filter(prb < 80 & thp > 2.7 & tad < 15 & lte > 80 & erf < -95 & cqi > 7 & dis == 0)
 
-top5_f <- path("data", "top5", ext = "fst")
-# top10_f <- path("data", "top10", ext = "fst")
-# write_fst(top5, path = top5_f, compress = 0)
-# write_fst(top10, path = top10_f, compress = 0)
+# AQUI ME QUEDE
+# 1. Corregir las etiquetas con base a las nuevas reglas
+# 2. Validar que ya no haya OPTIMIZACION
 
-top5 <- read_fst(top5_f) |> as_tibble()
-
-# 45571414 promotor
-# 38508957 capacidad
-# 46055729 disponibilidad
-# 53089094 optimización
-# 57522589 cobertura
-
-numeros <- c(45571414, 38508957, 46055729, 53089094, 57522589)
-
-
-muestra <- top5 |>
-  filter(user %in% numeros) |>
-  select(user, bts, date, met, hor, cll_prctg, prb:dis,
-         diag) |>
-  arrange(user, bts, met, hor) |>
+ctl_03 <- ctl_02 |>
+  drop_na() |>
+  filter(!(dis == 0 & diag == "DISPONIBILIDAD")) |>
   mutate(
-   across(cll_prctg, \(x) x * 100),
-   # across(diag, as.character),
-   diag2 = case_when(
-     prb > 85 & thp < 2.5 & tad < 15 ~ "CAPACIDAD",
-     prb > 85 & thp < 2.5 & tad > 15 ~ "COBERTURA",
-     .default = diag
-   )
-  )
-
-top5 |>
-  pivot_longer(prb:dfi, names_to = "metric", values_to = "value")
-
-top5 |>
-  filter(diag == "PROMOTOR", prb > 85 & thp < 2.5 & tad < 15)
-
-
-
-
-
-
-muestra |>
-  # filter(diag == "OPTIMIZACION") |>
-  arrange(-cll_prctg) |>
-  mutate(
-    across(diag, as.character),
     diag2 = case_when(
-    prb > 85 & thp < 2.5 & tad < 15 ~ "CAPACIDAD",
-    prb > 85 & thp < 2.5 & tad > 15 ~ "COBERTURA",
-    .default = diag
-   )
+      (prb > 80 & thp < 2.7 & tad < 15) | lte < 95     ~ "CAPACIDAD",
+      prb > 80 & thp < 2.7 & tad > 15  ~ "COBERTURA",
+      if_all(prb:vol, ~ .x == 0) & dis > 0 ~ "DISPONIBILIDAD",
+      prb < 80 & thp > 2.7 & tad < 15 & lte > 95 & erf < -95 & cqi > 7 & dis == 0 ~ "PROMOTOR",
+      cqi < 7 & psk > 33 ~ "OPTIMIZACION",
+      .default = diag
+    )
   )
 
+ctl_03 |> count(diag2)
 
-diag_capacity <- muestra |>
-  filter(user == 53089094)
-
-diag_capacity |>
-  arrange(-cll_prctg)
-
-diag_capacity |>
-  pivot_longer(prb:dif, names_to = "metric", values_to = "value") |>
-  ggplot(aes(x = value)) +
-  geom_histogram(bins = 50) +
-  facet_wrap(~ metric, scales = "free")
-
-diag_capacity |>
-  arrange(-cll_prctg) |>
-  ggplot(aes(x = date, y = prb)) +
-  geom_line(aes(color = bts))
-
-
-# ¿Cómo saber con cual de esta celda tomaron la decisión?
-diag_capacity |>
-  mutate(
-    eff = if_else(is.infinite(prb / thp), 0, (prb / thp)),
-    prb_alert = as.integer(prb > 80),
-    thp_alert = as.integer(thp < 2.5),
-    cap_alert = as.integer(eff > 32)) |>
-  relocate(diag, .after = last_col())
-  filter(cap_alert == 1)
-
-
-
-# ¿Cuántas de cada métrica?
-muestra |>
-  group_by(user) |>
-  summarise(across(c(twr, bts, met, hor), n_distinct))
-
-
-
-top5 |> count(diag)
-
-userx <- top5 |>
-  filter(user == 30001118, met <= first(met) + days(15))
-
-userx |>
-  count(twr, sort = TRUE)
-
-userx |>
-  ggplot(aes(y = fct_rev(fct_infreq(bts)))) +
-  geom_bar()
-
-userx |>
-  arrange(-cll_prctg) |>
-  count(cll_prctg)
-  ggplot(aes(x = cll_prctg)) +
-  geom_histogram(bins = 30)
-
-userx |>
-  filter(twr == "GUA181")
-  ggplot(aes(x = date, y = prb)) +
-  geom_point(aes(color = twr)) +
-  geom_line(aes(color = twr))
-  # scale_x_datetime(breaks = "hour")
-
-
-
-# Cargar dataset preprocesado --------------------------------------------------
-
-# 6.34 seg
-ctl_01 <- read_fst(cted_f) |> as_tibble()
+ctl_03 |> summarise(n = n_distinct(user), .by = diag2) |> mutate(prop = n / sum(n) * 100)
+ctl_03 |> filter(diag2 == "OPTIMIZACION") |> sample_n(size = 55) |> print(n = 55)
 
 
 ##  Resumir                                                                 ####
 
-# algunos filtros y transformaciones adicionales (pre-resumen)
-ctl_02 <- ctl_01 |>
- filter(
-  !if_all(prb:lod, ~ .x == 0),
-  !if_all(prb:thp, ~ .x == 0))
 
 tic()
 # 169.35 (2.82 min)
